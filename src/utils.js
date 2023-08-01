f34283d3ae7b9137d2bb063101e5b6f9f65a9b3d
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-01 22:41:50 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,9 @@ export function getRandomNumber(min, max) {
   const limit = max - min + 1
   return String.fromCharCode(Math.floor(Math.random() * limit) + min)
 }
+
+export function getSpecialChar() {
+  const specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+  return specialChar(Math.floor(Math.random * specialChar.length))
+}
